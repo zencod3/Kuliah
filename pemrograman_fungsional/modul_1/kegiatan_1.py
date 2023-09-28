94332d5887da9a903edd1441a0a277bef77fa76b
zencod3 <luthfizain45@gmail.com>
2023-09-28 11:30:44 +0700

    Upload Codelab Modul 1

---
+++ pemrograman_fungsional/modul_1/kegiatan_1.py
@@ -0,0 +1,59 @@
+# [] # fungsi pengurangan
+# def minus ():
+# # fungsi perkalian
+# def mult ():
+# # fungsi pembagian
+# def div ():
+
+# Fungsi penjumlahan
+def add(x, y):
+    return x + y
+
+# Fungsi pengurangan
+def minus(x, y):
+    return x - y
+
+# Fungsi perkalian
+def mult(x, y):
+    return x * y
+
+# Fungsi pembagian
+def div(x, y):
+    if y == 0:
+        return "Tidak dapat membagi dengan nol"
+    return x / y
+
+# Fungsi tree yang menggabungkan fungsi-fungsi aritmatik
+def tree(operation, *args):
+    if operation == "add":
+        result = 0
+        for arg in args:
+            result = add(result, arg)
+        return result
+    elif operation == "minus":
+        if len(args) < 2:
+            return "Kurang dari 2 operand untuk pengurangan"
+        result = args[0]
+        for i in range(1, len(args)):
+            result = minus(result, args[i])
+        return result
+    elif operation == "mult":
+        result = 1
+        for arg in args:
+            result = mult(result, arg)
+        return result
+    elif operation == "div":
+        if len(args) < 2:
+            return "Kurang dari 2 operand untuk pembagian"
+        result = args[0]
+        for i in range(1, len(args)):
+            result = div(result, args[i])
+        return result
+    else:
+        return "Operasi aritmatik tidak valid"
+
+# Contoh penggunaan fungsi tree
+print("Hasil = ", tree("add", 1, 2, 3))  # Output: 6
+print("Hasil = ", tree("minus", 10, 2, 3))  # Output: 5
+print("Hasil = ", tree("mult", 2, 3, 4))  # Output: 24
+print("Hasil = ", tree("div", 20, 2, 5))  # Output: 2.0
